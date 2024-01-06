44a7858bdb1a22b359832c5094d26771724a0d32
iLnsiC <besswalid@gmail.com>
2024-01-07 00:00:13 +0100

    [FIX] Front

--- front/assets/JS/script.js
+++ front/assets/JS/script.js
@@ -6,12 +6,87 @@ function redirect() {
         "/";
     window.location.replace(`${origin}login.html`);
 }
+
 function removeLoader() {
     const loader = document.querySelector('.loader-wrapper')
     loader.style.opacity = 0;
     setTimeout(() => {
         loader ? loader.remove() : null;
     }, 500);
+}
+
+function logOffAction() {
+    const logOffBtn = document.querySelector('.back-btn')
+    logOffBtn.addEventListener('click', () => {
+        sessionStorage.removeItem("token")
+        redirect();
+    })
+}
+
+function snackBar(message) {
+    const randId = 'snackbar-' + Math.round(Math.random() * 100000)
+    const snackbarHolder = document.querySelector('.snackbar-holder')
+    const snackbar = `<div id="${randId}" class="snack-bar">${message}</div>`
+    snackbarHolder.insertAdjacentHTML('beforeend', snackbar);
+    setTimeout(() => {
+        const snackbarEl = document.getElementById(randId)
+        snackbarEl.remove()
+    }, 5000);}
+
+function updateGuestAction(userId) {
+    const submitBtn = document.querySelector('#guest-update')
+    const form = document.querySelector('#presence-form')
+    submitBtn.addEventListener('click', (event)=>{
+        event.preventDefault();
+        const formData = new FormData(form)
+        let payload = {
+            diet: '',
+            restriction: '',
+            isPresent: {
+                cityHall: false,
+                cityHallValidatedBy: userId,
+                reception: false,
+                receptionValidatedBy: userId,
+            }
+        };
+        for (const [key, value] of formData) {
+            if (key == 'reception' || key == 'cityHall') {
+                payload.isPresent[key] = value == 'on' ? true : false
+            } else{
+                payload[key] = value
+            }
+        }
+        fetch(`http://localhost:3085/api/user/${userId}`, {
+            method: "PUT",
+            headers: {
+                "Content-Type": "application/json;charset=utf-8",
+                Authorization: `Bearer ${sessionStorage.token}`,
+            },
+            body: JSON.stringify(payload),
+        }).then((res) =>{
+            if (res.status === 200) {
+                snackBar('merci de nous avoir communiquer vos envies')
+            } else {
+                snackBar('Une erreur s\'est produite veuillez reessayer de nouveau <br> ou de communiquer directement avec nous')
+            }
+        }).catch(err => console.error(err))
+    })
+}
+
+function updateGroupAction(groupPresence) {
+    const submitBtn = document.querySelector('#group-update')
+    const inputs = document.querySelectorAll('#members input')
+    inputs.forEach(input => {
+        const [keyName, id] = input.id.split('-')
+        input.addEventListener('click', () => {
+            groupPresence[id][keyName] = input.checked
+        })
+    })
+    submitBtn.addEventListener('click', (event) => {
+        event.preventDefault()
+        const { userId, ...payload} = groupPresence
+
+    })
 }
 
 async function checkAuthorized() {
@@ -40,9 +115,25 @@ async function checkAuthorized() {
 
     return userId;
 }
+
 async function getUserData(userId) {
     let userData = null;
     await fetch(`http://localhost:3085/api/user/${userId}`, {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json;charset=utf-8",
+            Authorization: `Bearer ${sessionStorage.token}`,
+        },
+    })
+        .then(res => res.json())
+        .then(data => userData = data)
+        .catch(err => console.error(err))
+    return userData
+}
+
+async function getGroupMember(userId, groupId) {
+    let groupData = null;
+    await fetch(`http://localhost:3085/api/user/${userId}/group/${groupId}`, {
         method: "GET",
         headers: {
             "Content-Type": "application/json;charset=utf-8",
@@ -59,23 +150,23 @@ function loadHeader(userData) {
     const header = document.querySelector('header');
     const headerTemplate =
         `
-        <div class="header-title">
-            <h2>Save the date</h2>
-            <h1>Emilie & Walid</h1>
-        </div>
-        <img class="main-picture" src="assets/img/Groupe36.webp" alt="salle d'invitation" width="300"/>
-        <div class="header-text">
-            <span class="bold">${userData.firstName} ${userData.lastName}</span> <br>
-            Tu es invité au mariage de
-            <span class="bold">Walid et Emilie</span>
-            Qui se déroulera le
-            <span class="bold">20 avril 2024</span>
-            À partir de
-            <span class="bold">14h</span>
-            à la mairie d'<span class="bold">Ozoir-la-ferrière</span>
-        </div>
-        <img class="main-picture" src="assets/img/calendrier.webp" alt="salle d'invitation"/>
-    `
+            <div class="header-title">
+                <h2>Save the date</h2>
+                <h1>Emilie & Walid</h1>
+            </div>
+            <img class="main-picture" src="assets/img/Groupe36.webp" alt="salle d'invitation" width="300"/>
+            <div class="header-text">
+                <span class="bold">${userData.firstName} ${userData.lastName}</span> <br>
+                Tu es invité au mariage de
+                <span class="bold">Walid et Emilie</span>
+                Qui se déroulera le
+                <span class="bold">20 avril 2024</span>
+                À partir de
+                <span class="bold">14h</span>
+                à la mairie d'<span class="bold">Ozoir-la-ferrière</span>
+            </div>
+            <img class="main-picture" src="assets/img/calendrier.webp" alt="salle d'invitation"/>
+        `
     header.insertAdjacentHTML('beforeend', headerTemplate);
 }
 
@@ -131,7 +222,6 @@ function loadTheme(userData) {
 }
 
 function loadPresence(userData) {
-    console.log(userData.isPresent);
     const presenceSection = document.querySelector('#presence');
     const presenceTemplate =
         `
@@ -142,55 +232,71 @@ function loadPresence(userData) {
                 <div class="form-prencese">
                     <label for="cityHall">
                         À la mairie
-                        <input type="checkbox" name="cityHall" id="cityHall" ${userData.isPresent.cityHall ? 'checked' : null}/>
+                        <input type="checkbox" name="cityHall" id="cityHall" ${userData.isPresent.cityHall ? 'checked' : ''}/>
                     </label>
                     <label for="reception">
                         À la reception
-                        <input type="checkbox" name="reception" id="reception" ${userData.isPresent.reception ? 'checked' : null}/>
+                        <input type="checkbox" name="reception" id="reception" ${userData.isPresent.reception ? 'checked' : ''}/>
                     </label>
                 </div>
                 <div class="diet-select">
                     <label for="diet">Je suis un régime</label>
                     <select name="diet" id="diet">
                         <option value=""></option>
-                        <option value="Vegan">Végan</option>
-                        <option value="Vegetarien">Végétarien</option>
+                        <option value="vegan" ${userData.diet === 'vegan' ? 'selected': null}>Végan</option>
+                        <option value="vegetarien"  ${userData.diet === 'vegetarien' ? 'selected': null}>Végétarien</option>
                     </select>
                 </div>
                 <div class="form-textarea">
-                    <label for="moreInfo">J’ai des allergies ou un régime spécial <br>(Précisez le/léquels)</label>
-                    <textarea name="moreInfo" id="moreInfo" rows="5"></textarea>
+                    <label for="restriction">J’ai des allergies ou un régime spécial <br>(Précisez le/léquels)</label>
+                    <textarea name="restriction" id="restriction" rows="5">${userData.restriction}</textarea>
                 </div>
-                <button type="submit" class="main-btn">Valider</button>
+                <button id="guest-update" type="submit" class="main-btn">Valider</button>
             </form>
         </div>
     `
     presenceSection.insertAdjacentHTML('beforeend', presenceTemplate);
+    updateGuestAction(userData._id)
 }
 
-function loadGroup(userData) {
+function loadGroup(userData, groupData) {
     if (userData.isMain) {
+        let membreList = '';
+        let groupPresence = {
+            userId: userData._id
+        }
+        groupData.map(membre => {
+            groupPresence[membre.id] = {
+                cityHall: membre.isPresent.cityHall,
+                reception: membre.isPresent.reception,
+            }
+            membreList += `
+                <div class="member" id="id-user">
+                    <p>${membre.firstName} ${membre.lastName}</p>
+                    <label for="cityHall-${membre.id}">
+                        <input type="checkbox" name="cityHall-${membre.id}" id="cityHall-${membre.id}" ${membre.isPresent.cityHall ? 'checked' : ''}/>
+                        À la mairie
+                    </label>
+                    <label for="reception-${membre.id}">
+                        <input type="checkbox" name="reception-${membre.id}" id="reception-${membre.id}" ${membre.isPresent.reception ? 'checked' : ''}/>
+                        Au dîner
+                    </label>
+                </div>`
+        })
         const groupSection = document.querySelector('#group');
         const groupTemplate =
             `
-        <div class="wrapper">
-            <h2 class="section-title">Les membres de ma famille</h2>
-            <form id="members">
-                <div class="member" id="id-user">
-                    <p>Figuigui Ryad</p>
-                    <label for="cityHall">
-                        <input type="checkbox" name="cityHall" id="cityHall" />
-                        À la mairie
-                    </label>
-                    <label for="cityHall">
-                        <input type="checkbox" name="cityHall" id="cityHall" />
-                        Au dîner
-                    </label>
+                <div class="wrapper">
+                    <h2 class="section-title">Les membres de ma famille</h2>
+                    <h3 class="section-sub-title">Je valide leur presence (optionnel)</h3>
+                    <form id="members">
+                        ${membreList}
+                        <button id="group-update" class="main-btn" type="submit">Valider</button>
+                    </form>
                 </div>
-            </div>
-        </div>
-    `
+            `
         groupSection.insertAdjacentHTML('beforeend', groupTemplate);
+        updateGroupAction(groupPresence)
     }
 }
 
@@ -199,12 +305,14 @@ async function loadPage() {
     if (!userId) {
         redirect()
     }
+    logOffAction();
     const userData = await getUserData(userId);
+    const groupData = await getGroupMember(userId, userData.family)
     loadHeader(userData);
     loadPlanning(userData);
     loadTheme(userData);
     loadPresence(userData);
-    loadGroup(userData);
+    loadGroup(userData, groupData);
     removeLoader()
 }
 
